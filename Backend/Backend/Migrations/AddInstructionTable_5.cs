828fe295957ffcde493bb630dbfb499e17a6a077
YounesAbady <102363875+YounesAbady@users.noreply.github.com>
2022-09-01 19:54:09 +0200

    First Update
    Added Migrations
    Added the models with LLBLGen
    nothing much just testing

---
+++ Backend/Backend/Migrations/AddInstructionTable_5.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace Backend.Migrations
+{
+    [Migration(5)]
+    public class AddInstructionTable_5 : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("Instruction");
+        }
+
+        public override void Up()
+        {
+            Create.Table("Instruction")
+                .WithColumn("Id").AsGuid().NotNullable().PrimaryKey()
+                .WithColumn("Data").AsString().NotNullable()
+                .WithColumn("RecipeId").AsGuid().NotNullable().ForeignKey("Recipe", "Id");
+        }
+    }
+}
